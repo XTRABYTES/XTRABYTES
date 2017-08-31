8a4a269a29df22797d6166ad25fe99c69eda93dc
jcpbs <jenny01rr@gmail.com>
2017-08-31 12:38:46 +0100

    Checkpoint Updated

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -50,6 +50,7 @@ namespace Checkpoints
         ( 175000, uint256("0xadb4e2ee98127d185ec6e8e9762fc030e33ff579b16d1e146eac57bc31a1f57a"))
         ( 180000, uint256("0x9d71ec6abbd54a9e1afb59a5830f489c3a677b647c50a8f812d9db3542ec21b5"))
         ( 185000, uint256("0x849600d5a5d644a78960ce43792a5fb02a122ab33dda1ac243b72e46cba3a854"))
+        ( 190000, uint256("0x000000300dfe30b61c19177be4cd7e1328885305a00feb3228bf6cf54794c272"))
     ;
 
     // TestNet has no checkpoints
