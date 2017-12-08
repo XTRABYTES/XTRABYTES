ee9c68c9281f142cf55cd57fdccce390f68f61d8
jcpbs <jenny01rr@gmail.com>
2017-12-08 19:37:54 +0000

    Updated Code

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both xtrabytesd and xtrabytes-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("XtraBYtes");
+const std::string CLIENT_NAME("XTRABYTES");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   ""
