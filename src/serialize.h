1c650addf0895b76f00f3db6fe1406d85ec0f66a
jcpbs <jenny01rr@gmail.com>
2019-01-18 15:42:24 +0000

    OSX build fixes with qt5

--- src/serialize.h
+++ src/serialize.h
@@ -809,20 +809,7 @@ public:
     iterator insert(iterator it, const char& x=char()) { return vch.insert(it, x); }
     void insert(iterator it, size_type n, const char& x) { vch.insert(it, n, x); }
 
-#if !defined(_MSC_VER) || _MSC_VER >= 1300
-    void insert(iterator it, const_iterator first, const_iterator last)
-    {
-        assert(last - first >= 0);
-        if (it == vch.begin() + nReadPos && (unsigned int)(last - first) <= nReadPos)
-        {
-            // special case for inserting at the front when there's room
-            nReadPos -= (last - first);
-            memcpy(&vch[nReadPos], &first[0], last - first);
-        }
-        else
-            vch.insert(it, first, last);
-    }
-#endif
+
 
     void insert(iterator it, std::vector<char>::const_iterator first, std::vector<char>::const_iterator last)
     {
